51ce392d18ab69831f133561b716bfcd7c0567dd
elsaD01 <113036495+elsaD01@users.noreply.github.com>
2023-03-02 15:18:01 +0100

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; 
+
+class Main{
+
+	 public static void main(String [] args){
+
+	 	String question = "Please type your name ";
+
+	 	String input = name; 
+
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+
+
+		
+
+
+
+
+	}
+
+
+
+
+
+}
