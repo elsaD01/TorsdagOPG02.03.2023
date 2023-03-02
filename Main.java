06268a9357156a561299150be0d718da92daade6
elsaD01 <113036495+elsaD01@users.noreply.github.com>
2023-03-02 15:17:27 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,18 @@
-import java.util.Scanner; 
-
 class Main{
+	
+	public static void main(String [] args){
 
-	 public static void main(String [] args){
+		TeamName tn = new TeamName();
+		tn.teamName = "TheGoats";
+		String teamName = tn.getName();
+		System.out.println("The teams name is: " + teamName);
 
-	 	String question = "Please type your name ";
-
-	 	String input = name; 
-
-		Scanner scan = new Scanner(System.in);
-		String input = scan.nextLine();
-
-
-		
-
-
-
+		SetRank sr = new SetRank();
+		int age = Integer.parseInt(sr.setRank("Teams rank is: "));
+		System.out.println(age);
+	 
+	
 
 	}
-
-
-
-
 
 }
